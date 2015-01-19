1f5e8c3f8049ebf01124efdfc88408384f65c6db
Ankush <ankushgaba20@gmail.com>
2015-01-19 09:58:51 +0530

    GitDemo update @pankaj's System

--- GitDemo/src/com/git/example/GitExample.java
+++ GitDemo/src/com/git/example/GitExample.java
@@ -8,6 +8,7 @@ public static void main(String[] args)
 	System.out.println("Ashish Demonstrating Git.....");
 	System.out.println(" Demonstrating Git to team.....");
 	System.out.println("Third Git to team.....");
+	System.out.println("Ankush is now  Git to team.....");
 }
 }
 
